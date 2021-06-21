ef21f1681345d9020c6c2d89eb0dafdcba9e34f1
Tadas Bruzas <tadasbruzas91@gmail.com>
2021-06-21 19:46:28 +0300

    Fix trip data screen bugs.

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@ int addressTrip = 5;
 int addressTripDriveTime = 10;
 int addressTripAvgSpeed = 15;
 int addressTripIdleTime = 20;
+int addressTripStartTime = 25;
 
 int screenRotation = 1;
 
@@ -33,6 +34,7 @@ unsigned int circMetric = 206; // wheel circumference (in centimeters)
 unsigned int speedk;           // holds calculated speed vales in metric
 unsigned int distance = 0;
 unsigned int odometer = 0;
+unsigned long tripStartTime = 0;
 unsigned long tripDriveTime = 0;
 float tripDriveAvgSpeed = 0.00f;
 unsigned long tripIdleTime = 0;
@@ -57,6 +59,7 @@ void displayTime();
 void displayTemp();
 void displayOdo();
 void displayTrip();
+void displayTripStart();
 void displayTripDriveTime();
 void displayTripDriveAvgSpeed();
 void displayTripIdleTime();
@@ -103,6 +106,7 @@ void setup()
 void loop()
 {
   displayView();
+  changeView();
 
   calculateDriveTime();
   calculateIdleTime();
@@ -142,8 +146,10 @@ void resetDistance()
   {
     if (millis() - distanceRstTime > 3000)
     {
+      DateTime now = rtc.now();
       distance = tripDriveTime = tripIdleTime = 0;
       tripDriveAvgSpeed = 0.00f;
+      tripStartTime = (now.hour() * 3600000) + (now.minute() * 60000) + (now.second() * 1000);
       savedToEeprom = false;
     }
   }
@@ -169,6 +175,15 @@ void calculateIdleTime()
   }
 }
 
+void displayTime()
+{
+  DateTime now = rtc.now();
+  tft.setCursor(5, 5);
+  tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+  tft.setTextFont(4);
+  tft.printf("%02d:%02d", now.hour(), now.minute());
+}
+
 void displaySpeed()
 {
   int speedPos = 100;
@@ -185,14 +200,6 @@ void displaySpeed()
   tft.drawString(String(meterph), speedPos + 114, 80, 6);
 }
 
-void displayTime()
-{
-  DateTime now = rtc.now();
-  tft.setCursor(5, 5);
-  tft.setTextColor(TFT_YELLOW, TFT_BLACK);
-  tft.printf("%02d:%02d", now.hour(), now.minute());
-}
-
 void displayTemp()
 {
   if (millis() - tempUpdated > 5000 || afterStartTemp)
@@ -200,6 +207,7 @@ void displayTemp()
     String temp = String(bme.readTemperature(), 1);
     tft.setCursor(230, 5);
     tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+    tft.setTextFont(4);
     tft.print(temp);
     tft.print(" C");
     tempUpdated = millis();
@@ -237,14 +245,27 @@ void displayTrip()
   tft.printf("%03d", m100);
 }
 
-void displayTripDriveTime()
+void displayTripStart()
 {
-  MsConverter time(tripDriveTime);
+  MsConverter time(tripStartTime);
 
   tft.setCursor(5, 5);
   tft.setTextColor(TFT_YELLOW, TFT_BLACK);
   tft.setTextFont(4);
+  tft.print("Trip start ");
+  tft.setTextColor(TFT_RED,TFT_BLACK);
+  tft.print(time.getTimeString());
+}
+
+void displayTripDriveTime()
+{
+  MsConverter time(tripDriveTime);
+
+  tft.setCursor(5, 45);
+  tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+  tft.setTextFont(4);
   tft.print("Drive time ");
+  tft.setTextColor(TFT_RED,TFT_BLACK);
   tft.print(time.getTimeString());
 }
 
@@ -252,11 +273,14 @@ void displayTripDriveAvgSpeed()
 {
   float time = tripDriveTime / 3600000.0;
   float avgSpeed = (distance / 100000.0) / time;
-  tft.setCursor(5, 45);
+  if(distance == 0) avgSpeed = 0.0;
+  tft.setCursor(5, 85);
   tft.setTextColor(TFT_YELLOW, TFT_BLACK);
   tft.setTextFont(4);
   tft.print("Avg speed ");
+  tft.setTextColor(TFT_RED,TFT_BLACK);
   tft.print(avgSpeed, 1);
+  tft.setTextColor(TFT_YELLOW, TFT_BLACK);
   tft.print(" km/h");
 }
 
@@ -264,10 +288,11 @@ void displayTripIdleTime()
 {
   MsConverter time(tripIdleTime);
 
-  tft.setCursor(5, 85);
+  tft.setCursor(5, 125);
   tft.setTextColor(TFT_YELLOW, TFT_BLACK);
   tft.setTextFont(4);
   tft.print("Idle time ");
+  tft.setTextColor(TFT_RED,TFT_BLACK);
   tft.print(time.getTimeString());
 }
 
@@ -278,6 +303,7 @@ void getDataFromEeprom()
   EEPROM.get(addressTripDriveTime, tripDriveTime);
   EEPROM.get(addressTripAvgSpeed, tripDriveAvgSpeed);
   EEPROM.get(addressTripIdleTime, tripIdleTime);
+  EEPROM.get(addressTripStartTime, tripStartTime);
 }
 
 void writeDataToEeprom()
@@ -291,6 +317,7 @@ void writeDataToEeprom()
     EEPROM.put(addressTripDriveTime, tripDriveTime);
     EEPROM.put(addressTripAvgSpeed, tripDriveAvgSpeed);
     EEPROM.put(addressTripIdleTime, tripIdleTime);
+    EEPROM.put(addressTripStartTime, tripStartTime);
     savedToEeprom = true;
   }
 }
@@ -306,6 +333,7 @@ void mainScreen()
 
 void tripDataScreen()
 {
+  displayTripStart();
   displayTripDriveTime();
   displayTripDriveAvgSpeed();
   displayTripIdleTime();
@@ -326,6 +354,7 @@ void changeView()
       if (screenSelector < 1)
         screenSelector = scrensAvailable;
       screenChangeTime = millis();
+      tft.fillScreen(TFT_BLACK);
       displayView();
     }
   }
